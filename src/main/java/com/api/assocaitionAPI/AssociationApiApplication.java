3779d8566b462028554f8b834f44e9351db23e88
Erfan Bohloul <erfanbohloul@gmail.com>
2024-09-22 20:04:29 +0330

    first commit

---
+++ src/main/java/com/api/assocaitionAPI/AssociationApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.assocaitionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociationApiApplication.class, args);
+	}
+
+}
